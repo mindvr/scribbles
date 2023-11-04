b6348a548b75bd1aa3a5deb81bc8d23edfbb1ffb
Stepan Kuznetsov <mindvr@gmail.com>
2023-11-04 10:51:56 +0600

    add flyway dependency

--- flyway/src/main/java/com/github/mindvr/Main.java
+++ flyway/src/main/java/com/github/mindvr/Main.java
@@ -1,7 +1,19 @@
 package com.github.mindvr;
 
+import org.flywaydb.core.Flyway;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Configure Flyway with SQLite database
+        Flyway flyway = Flyway.configure()
+                .dataSource("jdbc:sqlite:sample.db", "", "")
+
+                .load();
+
+        // Start the migration
+        flyway.migrate();
+
+
+        System.out.println("Migration successful!");
     }
 }
