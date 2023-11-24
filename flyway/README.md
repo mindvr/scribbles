1614e4ebda6be48085eaad09997c0a5060d4bf72
Stepan Kuznetsov <mindvr@gmail.com>
2023-11-24 12:51:50 +0600

    failure mode 5

--- flyway/README.md
+++ flyway/README.md
@@ -9,4 +9,4 @@ Failure modes:
 2. checksum changes: v1 -> f2
 3. baseline only + existing flyway log v1 -> f3 (compared to --clear f3)
 4. sql fail v1 -> f4, check db
-5. java fail
+5. java fail v1 -> f5, check db (transaction per migration)
---
+++ flyway/src/main/resources/migrations/f5/V3__add_null_emp.sql
@@ -0,0 +1,5 @@
+INSERT INTO emp(empno, ename, job, mgr, hiredate, sal, deptno)
+values (7788, 'SCOTT', 'ANALYST', 7566, NULL, 3000, 20);
+
+INSERT INTO emp(empno, ename, job, mgr, hiredate, sal, deptno)
+values (7902, 'FORD', 'ANALYST', 7566, null, 3000, 20);
