99efe954f7f0f3d149070f06f8866563f9a0b4e6
Stepan Kuznetsov <mindvr@gmail.com>
2023-12-02 18:10:34 +0600

    move core logic out of main

--- zero-ballance/main.py
+++ zero-ballance/main.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
-def permutate(base, base_max, limit):
-    if base_max[0] == -1:
+def permutate(bases, base_limits, limit):
+    if base_limits[0] == -1:
         eff_limit = limit
     else:
-        eff_limit = min(base[0] * base_max[0], limit)
+        eff_limit = min(bases[0] * base_limits[0], limit)
 
-    if len(base) == 1:
-        return list(range(0, eff_limit // base[0] + 1))
+    if len(bases) == 1:
+        return list(range(0, eff_limit // bases[0] + 1))
 
     vectors = []
-    for x in range(0, eff_limit // base[0] + 1):
-        for v in permutate(base[1:], base_max[1:], limit - x * base[0]):
+    for x in range(0, eff_limit // bases[0] + 1):
+        for v in permutate(bases[1:], base_limits[1:], limit - x * bases[0]):
             if isinstance(v, list):
                 vectors.append([x] + v)
             elif isinstance(v, int):
@@ -21,15 +21,29 @@ def permutate(base, base_max, limit):
     return vectors
 
 
-def v_length(base, v):
-    if isinstance(v, int):
-        return base[0] * v
-    elif isinstance(v, list):
-        assert len(base) == len(v)
-        x = [i * j for i, j in zip(base, v)]
+def v_length(bases, vector):
+    if isinstance(vector, int):
+        return bases[0] * vector
+    elif isinstance(vector, list):
+        assert len(bases) == len(vector)
+        x = [i * j for i, j in zip(bases, vector)]
         return sum(x)
     else:
         raise TypeError()
+
+
+def ways_to_zero(bases, base_limits, exact, min_add, max_add):
+    assert len(bases) == len(base_limits)
+    limit = exact + max_add
+    permutations = permutate(bases, base_limits, limit)
+    permutations.reverse()
+
+    def match_criterion(v):
+        left = exact + min_add
+        l = v_length(v, bases)
+        return l == exact or left <= l < limit
+
+    return [((v_length(bases, v), v_length(bases, v) - exact), tuple(v)) for v in permutations if match_criterion(v)]
 
 
 # TODOs:
@@ -38,37 +52,9 @@ def v_length(base, v):
 # 6. readme
 
 if __name__ == '__main__':
-
-    base = [449, 199,  # nPlayer, PDD
-            15, 99, 199, 249, 349, 449, 499, 699, # bullshit apps
-            # 59, 149, 599,  # icloud 50G 200G and 2000G
-            # 399,  # Podcasts
-            # 449, 899,  # Duo Donates
-            ]
-    base_max = [1, 1,
-                1, 5, 5, 5, 3, 3, 2, 1,
-                # 1, 1, 1,
-                # 1,
-                # 1, 1
-                ]
+    base = [449, 199, 15, 99, 199, 249, 349, 449, 499, 699]
+    base_max = [1, 1, 1, 5, 5, 5, 3, 3, 2, 1]
     s_exact, add_min, add_max = 779, 500, 550
-
-    limit = s_exact + add_max
-    assert len(base) == len(base_max)
-    permutations = permutate(base, base_max, limit)
-    permutations.reverse()
-    # permutations.sort(key=lambda x: v_length(base, x))
-
-
-    def match_criterion(v):
-        left = s_exact + add_min
-        l = v_length(v, base)
-        return l == s_exact or left <= l < limit
-
-
-    matches = [v for v in permutations if match_criterion(v)]
-
     print(base)
-    for v in matches:
-        l = v_length(base, v)
-        print(v, l, l - s_exact)
+    for v in ways_to_zero(base, base_max, s_exact, add_min, add_max):
+        print(v)
