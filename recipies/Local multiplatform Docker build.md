998faa1cccec818ebf9c5e07dca500a3bb8be3f7
Stepan Kuznetsov <mindvr@gmail.com>
2023-12-17 21:01:11 +0600

    add non buildx multiplatform builder

--- recipies/Local multiplatform Docker build.md
+++ recipies/Local multiplatform Docker build.md
@@ -1,5 +1,10 @@
 # Local multiplatform Docker build
 
+Running buildx build+push is once commmand, but doesn't play well with self signed certificates, 
+since it runs new container that requires pushing CA inside of it. 
+
+Doing multiplatform build w/o buildx requires more commands but otherwise is simpler: 
+
 ```sh
 docker build --platform linux/amd64 -t repository-url/image-name:amd64 .
 docker build --platform linux/arm64 -t repository-url/image-name:arm64 .
@@ -12,4 +17,12 @@ docker manifest annotate repository-url/image-name repository-url/image-name:amd
 docker manifest annotate repository-url/image-name repository-url/image-name:arm64 --arch arm64
 
 docker manifest push repository-url/image-name
+```
+
+[buildy.sh](a/buildy.sh) - as a script with sensible defaults to build amd64 and arm64:
+
+```
+-i|--image-name - image name, default - current directory name
+-r|--repository-url - repository url, BUILDY_REPOSITORY_URL env variable by default
+-t|--tag - image tag, latest by default
 ```
---
+++ recipies/a/buildy.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Default values
+default_image_name=$(basename "$(pwd)")
+default_repository_url=${BUILDY_REPOSITORY_URL}
+default_tag="latest"
+
+# Parse CLI arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -i|--image-name) image_name="$2"; shift ;;
+        -r|--repository-url) repository_url="$2"; shift ;;
+        -t|--tag) tag="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Set defaults if not provided
+image_name=${image_name:-$default_image_name}
+repository_url=${repository_url:-$default_repository_url}
+tag=${tag:-$default_tag}
+
+# Check if repository URL is provided
+if [ -z "$repository_url" ]; then
+    echo "Error: Repository URL not provided and BUILDY_REPOSITORY_URL is not set."
+    exit 1
+fi
+
+# Building and pushing for amd64
+docker build --platform linux/amd64 -t $repository_url/$image_name:amd64-$tag .
+docker push $repository_url/$image_name:amd64-$tag
+
+# Building and pushing for arm64
+docker build --platform linux/arm64 -t $repository_url/$image_name:arm64-$tag .
+docker push $repository_url/$image_name:arm64-$tag
+
+# Creating and pushing the manifest
+docker manifest create $repository_url/$image_name:$tag \
+    $repository_url/$image_name:amd64-$tag \
+    $repository_url/$image_name:arm64-$tag
+
+docker manifest annotate $repository_url/$image_name:$tag \
+    $repository_url/$image_name:amd64-$tag --arch amd64
+
+docker manifest annotate $repository_url/$image_name:$tag \
+    $repository_url/$image_name:arm64-$tag --arch arm64
+
+docker manifest push $repository_url/$image_name:$tag
+
+echo "Build and push complete."
