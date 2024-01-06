3e3cf13316c3539e00ffac627e42325719f591fc
Stepan Kuznetsov <mindvr@gmail.com>
2024-01-06 20:48:37 +0600

    budilniks 80% done

---
+++ y-cup-examples/budilniks/src/main/java/dev/mindvr/Main.java
@@ -0,0 +1,11 @@
+package dev.mindvr;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(-5/5);
+        System.out.println(0/5);
+        System.out.println(1/5);
+        System.out.println(5/5);
+    }
+}
---
+++ y-cup-examples/budilniks/src/main/java/dev/mindvr/Tabulator.java
@@ -0,0 +1,62 @@
+package dev.mindvr;
+
+import java.util.Arrays;
+
+public class Tabulator {
+    private int step(long v) {
+        return v < 0 ? 0 : 1;
+    }
+
+    public long sumSingle(long t, long tStart, long xIncr) {
+        return step(t - tStart) + (t - tStart) / xIncr;
+    }
+
+    // input is presumed to be pre-processed
+    public long findT(int x, int k, int[] tI) {
+        long l = 0;
+        long r = tI[tI.length - 1] + (long) k * x / (tI.length);
+        long tRes = -1;
+        long t, vT;
+        while (l <= r) {
+            t = l + (r - l) / 2;
+            vT = sumTotal(t, tI, x);
+            if (vT == k) {
+                tRes = t;
+                r = t - 1;
+            } else if (vT < k) {
+                l = t + 1;
+            } else {
+                r = t - 1;
+            }
+        }
+        return tRes;
+    }
+
+    public long sumTotal(long t, int[] tI, int x) {
+        int m = findLeqIndex(tI, t) + 1;
+        long sum = m;
+        for (int i = 0; i < m; i++) {
+            sum += (t - tI[i]) / x;
+        }
+        return sum;
+    }
+
+
+    int findLeqIndex(int[] sortedUnique, long value) {
+        int l, r, m, res;
+        l = 0;
+        res = -1;
+        r = sortedUnique.length - 1;
+        while (l <= r) {
+            m = l + (r - l) / 2;
+            if (sortedUnique[m] <= value) {
+                res = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+
+        }
+        return res;
+    }
+}
